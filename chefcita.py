2efd01dd2427437dfabef0205ac7df6f173a7f83
cedioza <cdioza@smartdataautomation.com>
2023-11-26 20:37:12 -0500

    first commit

---
+++ chefcita.py
@@ -0,0 +1,19 @@
+
+variable1 = "hola"
+variable2 = "mundo"
+variable2 = 5
+variable2 = 2.56
+variable2 = {
+    "llave":"valor"
+}
+variable2 = [1,2,3,4,5,6,7,8]
+
+
+
+
+print(variable1)
+print(variable2)
+
+
+
+# print("quien es una chefcita toda divina ")
